269b4112f472a3a0632688fc032ad5fe80446c9f
Todd Leach <8894107+Jad-KMC@users.noreply.github.com>
2025-06-18 14:29:23 -0400

    Update PoolDataSubmit.js
    convert from pact-lang-api to KadenaJS

--- PoolDataSubmit.js
+++ PoolDataSubmit.js
@@ -1,8 +1,11 @@
-// combinedScript.js
-
 import { URL } from 'url';
 import { promises as fs } from 'fs';
-import Pact from 'pact-lang-api';
+import {
+    Pact,
+    createClient,
+    createSignWithKeypair,
+    isSignedTransaction
+} from '@kadena/client'
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,23 +21,23 @@ const apiKey = process.env.VIABTC_API_KEY;
  * @returns {Promise<{balance: any, paid: any}>} - Resolves with an object containing balance and paid.
  */
 export async function fetchPoolFlareStats() {
-  const statsUrl = 'https://poolflare.net/api/v1/coin/kda/account/k:bc14a24516fba3a6dcc1f6f54ab16e10213c7dbd85f220ceb95043b72c3595df/stats';
+    const statsUrl = 'https://poolflare.net/api/v1/coin/kda/account/k:bc14a24516fba3a6dcc1f6f54ab16e10213c7dbd85f220ceb95043b72c3595df/stats';
 
-  try {
-    const response = await fetch(statsUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+        const response = await fetch(statsUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const statsData = await response.json();
+        if (!statsData || !statsData.status) {
+            throw new Error("Invalid stats data or status is false.");
+        }
+        const { balance, paid } = statsData.data;
+        return { balance, paid };
+    } catch (error) {
+        console.error("Error fetching stats data:", error);
+        throw error;
     }
-    const statsData = await response.json();
-    if (!statsData || !statsData.status) {
-      throw new Error("Invalid stats data or status is false.");
-    }
-    const { balance, paid } = statsData.data;
-    return { balance, paid };
-  } catch (error) {
-    console.error("Error fetching stats data:", error);
-    throw error;
-  }
 }
 
 /**
@@ -43,30 +46,30 @@ export async function fetchPoolFlareStats() {
  * @returns {Promise<number>} - Resolves with the total profit number.
  */
 export async function fetchProfitSummary(coin) {
-  const url = new URL('/res/openapi/v1/profit', baseUrl);
-  url.searchParams.append('coin', coin);
+    const url = new URL('/res/openapi/v1/profit', baseUrl);
+    url.searchParams.append('coin', coin);
 
-  try {
-    const headers = {
-      'X-API-KEY': apiKey,
-      'Content-Type': 'application/json'
-    };
+    try {
+        const headers = {
+            'X-API-KEY': apiKey,
+            'Content-Type': 'application/json'
+        };
 
-    const response = await fetch(url, { headers });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        const response = await fetch(url, { headers });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        if (data.code === 0) {
+            return data.data.total_profit;
+        } else {
+            throw new Error(`API error: ${data.message}`);
+        }
+    } catch (error) {
+        console.error(`Error fetching ${coin} data:`, error);
+        throw error;
     }
-
-    const data = await response.json();
-    if (data.code === 0) {
-      return data.data.total_profit;
-    } else {
-      throw new Error(`API error: ${data.message}`);
-    }
-  } catch (error) {
-    console.error(`Error fetching ${coin} data:`, error);
-    throw error;
-  }
 }
 
 /**
@@ -74,64 +77,52 @@ export async function fetchProfitSummary(coin) {
  * @returns {Promise<number>} - Resolves with the total DOGE rewards.
  */
 export async function fetchTotalDogeRewards() {
-  const url = new URL('/res/openapi/v1/reward/history', baseUrl);
-  url.searchParams.append('coin', 'DOGE');
+    const url = new URL('/res/openapi/v1/reward/history', baseUrl);
+    url.searchParams.append('coin', 'DOGE');
 
-  // Fixed start date and dynamic end date.
-  const fixedStartDate = '2024-02-17';
-  const today = new Date();
-  const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  };
-
-  url.searchParams.append('start_date', fixedStartDate);
-  url.searchParams.append('end_date', formatDate(today));
-  url.searchParams.append('limit', '100');
-
-  try {
-    const headers = {
-      'X-API-KEY': apiKey
+    // Fixed start date and dynamic end date.
+    const fixedStartDate = '2024-02-17';
+    const today = new Date();
+    const formatDate = (date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
     };
 
-    const response = await fetch(url, { headers });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+    url.searchParams.append('start_date', fixedStartDate);
+    url.searchParams.append('end_date', formatDate(today));
+    url.searchParams.append('limit', '100');
 
-    const data = await response.json();
-    if (data.code === 0) {
-      let totalRewards = 0;
-      data.data.data.forEach(day => {
-        totalRewards += parseFloat(day.total_profit);
-      });
-      return totalRewards;
-    } else {
-      throw new Error(`API error: ${data.message}`);
+    try {
+        const headers = {
+            'X-API-KEY': apiKey
+        };
+
+        const response = await fetch(url, { headers });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        if (data.code === 0) {
+            let totalRewards = 0;
+            data.data.data.forEach(day => {
+                totalRewards += parseFloat(day.total_profit);
+            });
+            return totalRewards;
+        } else {
+            throw new Error(`API error: ${data.message}`);
+        }
+    } catch (error) {
+        console.error('Error fetching DOGE data:', error);
+        throw error;
     }
-  } catch (error) {
-    console.error('Error fetching DOGE data:', error);
-    throw error;
-  }
 }
 
 // ======================================================
 // Pact / Chainweb Functions
 // ======================================================
-
-const NETWORK_ID = process.env.NETWORK_ID;
-const CHAIN_ID = process.env.CHAIN_ID;
-const API = process.env.API;
-const API_HOST = `https://${API}/chainweb/0.0/${NETWORK_ID}/chain/${CHAIN_ID}/pact`;
-
-const KEY_PAIR = {
-  publicKey: process.env.PUBLIC_KEY,
-  secretKey: process.env.SECRET_KEY,
-};
-
-const creationTime = () => Math.round(new Date().getTime() / 1000);
 
 /**
  * Format the price value.
@@ -140,25 +131,37 @@ const creationTime = () => Math.round(new Date().getTime() / 1000);
  * @returns {string} - The formatted number as a string.
  */
 const formatPrice = (value, round) => {
-  const stringValue = value.toString();
-  let decimalNumber;
-  try {
-    let numValue = Number(stringValue);
+    const stringValue = value.toString();
+    let decimalNumber;
+    try {
+        let numValue = Number(stringValue);
 
-    if (round) {
-      decimalNumber = Math.ceil(numValue) + 0.0000001;
-    } else {
-      if (Number.isInteger(numValue)) {
-        decimalNumber = numValue.toFixed(1);
-      } else {
-        decimalNumber = numValue.toFixed(5);
-      }
+        if (round) {
+            decimalNumber = Math.ceil(numValue) + 0.0000001;
+        } else {
+            if (Number.isInteger(numValue)) {
+                decimalNumber = numValue.toFixed(1);
+            } else {
+                decimalNumber = numValue.toFixed(5);
+            }
+        }
+    } catch (e) {
+        decimalNumber = stringValue;
     }
-  } catch (e) {
-    decimalNumber = stringValue;
-  }
 
-  return decimalNumber;
+    return decimalNumber;
+};
+
+
+const NETWORK_ID = process.env.NETWORK_ID;
+const CHAIN_ID = process.env.CHAIN_ID;
+const API = process.env.API;
+const API_HOST = `https://${API}/chainweb/0.0/${NETWORK_ID}/chain/${CHAIN_ID}/pact`;
+const CLIENT = createClient(API_HOST)
+
+const KEY_PAIR = {
+    publicKey: process.env.PUBLIC_KEY,
+    secretKey: process.env.SECRET_KEY,
 };
 
 /**
@@ -169,23 +172,63 @@ const formatPrice = (value, round) => {
  * @returns {Promise<any>} - Resolves with the API response.
  */
 async function setMined(coin, mined, caller) {
-  const cmd = {
-    networkId: NETWORK_ID,
-    keyPairs: KEY_PAIR,
-    pactCode: `(n_e98a056e3e14203e6ec18fada427334b21b667d8.chips.insert-coin-mined "${coin}" ${mined} "${caller}")`,
-    envData: {},
-    meta: {
-      creationTime: creationTime(),
-      ttl: 28000,
-      gasLimit: 10000,
-      chainId: CHAIN_ID,
-      gasPrice: 0.0000001,
-      sender: "k:" + KEY_PAIR.publicKey,
-    },
-  };
+    const contract = 'n_e98a056e3e14203e6ec18fada427334b21b667d8.chips'
+    const publicKey = process.env.PUBLIC_KEY
 
-  const response = await Pact.fetch.local(cmd, API_HOST);
-  return response;
+    const signAccount = `k:${process.env.PUBLIC_KEY}`
+    const signWithKeypair = createSignWithKeypair(KEY_PAIR)
+    const transactionBuilder = Pact.modules[contract]['insert-coin-mined'](coin, mined, caller)
+
+    const unsignedTransaction = Pact.builder
+        .execution(transactionBuilder)
+        .setMeta({
+            chainId: CHAIN_ID,
+            senderAccount: signAccount,
+            gasLimit: 10000
+        })
+        .addSigner(publicKey, (signFor) => [
+            signFor('coin.GAS'),
+            signFor(`${contract}.insert-coin-mined`),
+            signFor(`${contract}.ACCOUNT_GUARD`, signAccount)
+        ])
+        .addData('account', signAccount)
+        .setNetworkId(NETWORK_ID)
+        .createTransaction()
+
+    let signedTx
+    try {
+        signedTx = await signWithKeypair(unsignedTransaction)
+        console.log('Signed transaction:', JSON.stringify(signedTx))
+    } catch (error) {
+        console.error('Error signing transaction:', error)
+        return
+    }
+
+    // Prepare the payload for preflight
+    console.log('signedTx', signedTx)
+    try {
+        // Perform a preflight check
+        const preflightResult = await CLIENT.preflight(signedTx)
+        console.log('Preflight result:', preflightResult)
+
+        if (preflightResult?.result?.status === 'failure') {
+            console.error('Preflight failure:', preflightResult.result.status)
+        }
+
+        console.log('Preflight successful')
+
+        // Submit the transaction if preflight is successful
+        if (isSignedTransaction(signedTx)) {
+            const transactionDescriptor = await CLIENT.submit(signedTx)
+            console.log(
+                'transactionDescriptor.requestKey',
+                transactionDescriptor.requestKey
+            )
+            return transactionDescriptor.requestKey
+        }
+    } catch (error) {
+        console.error('Error during preflight or submission:', error)
+    }
 }
 
 // ======================================================
@@ -193,53 +236,53 @@ async function setMined(coin, mined, caller) {
 // ======================================================
 
 const updateMined = async () => {
-  while (true) {
-    try {
-      // 1. PoolFlare Stats for KDA:
-      const { balance, paid } = await fetchPoolFlareStats();
-      // Convert balance and paid to numbers and sum them
-      const kdaMined = parseFloat(balance) + parseFloat(paid);
-      const kdaResult = await setMined(
-        "KDA",
-        formatPrice(kdaMined),
-        "k:" + KEY_PAIR.publicKey
-      );
-      console.log("KDA result:", kdaResult);
+    while (true) {
+        try {
+            // 1. PoolFlare Stats for KDA:
+            const { balance, paid } = await fetchPoolFlareStats();
+            // Convert balance and paid to numbers and sum them
+            const kdaMined = parseFloat(balance) + parseFloat(paid);
+            const kdaResult = await setMined(
+                "KDA",
+                formatPrice(kdaMined),
+                "k:" + KEY_PAIR.publicKey
+            );
+            console.log("KDA result:", kdaResult);
 
-      // 2. BTC Profit Summary:
-      const btcProfit = await fetchProfitSummary("BTC");
-      // Force add 0.1
-      const adjustedBtcProfit = Number(btcProfit);
-      const btcResult = await setMined("BTC", formatPrice(adjustedBtcProfit), "k:" + KEY_PAIR.publicKey);
-      
-      
-      console.log("BTC result:", btcResult);
+            // 2. BTC Profit Summary:
+            const btcProfit = await fetchProfitSummary("BTC");
+            // Force add 0.1
+            const adjustedBtcProfit = Number(btcProfit);
+            const btcResult = await setMined("BTC", formatPrice(adjustedBtcProfit), "k:" + KEY_PAIR.publicKey);
 
-      // 3. LTC Profit Summary:
-      const ltcProfit = await fetchProfitSummary("LTC");
-      const adjustedLtcProfit = Number(ltcProfit);
-      const ltcResult = await setMined(
-        "LTC",
-        formatPrice(adjustedLtcProfit),
-        "k:" + KEY_PAIR.publicKey
-      );
-      console.log("LTC result:", ltcResult);
 
-      // 4. DOGE Profit Summary (via reward history):
-      const dogeProfit = await fetchTotalDogeRewards();
-      const dogeResult = await setMined(
-        "DOGE",
-        formatPrice(dogeProfit),
-        "k:" + KEY_PAIR.publicKey
-      );
-      console.log("DOGE result:", dogeResult);
+            console.log("BTC result:", btcResult);
 
-      // Wait for two hours (6600000 ms) before the next iteration.
-      await new Promise((resolve) => setTimeout(resolve, 6600000));
-    } catch (error) {
-      console.error("Error in update loop:", error);
+            // 3. LTC Profit Summary:
+            const ltcProfit = await fetchProfitSummary("LTC");
+            const adjustedLtcProfit = Number(ltcProfit);
+            const ltcResult = await setMined(
+                "LTC",
+                formatPrice(adjustedLtcProfit),
+                "k:" + KEY_PAIR.publicKey
+            );
+            console.log("LTC result:", ltcResult);
+
+            // 4. DOGE Profit Summary (via reward history):
+            const dogeProfit = await fetchTotalDogeRewards();
+            const dogeResult = await setMined(
+                "DOGE",
+                formatPrice(dogeProfit),
+                "k:" + KEY_PAIR.publicKey
+            );
+            console.log("DOGE result:", dogeResult);
+
+            // Wait for two hours (6600000 ms) before the next iteration.
+            await new Promise((resolve) => setTimeout(resolve, 6600000));
+        } catch (error) {
+            console.error("Error in update loop:", error);
+        }
     }
-  }
 };
 
 updateMined();
